0b6ff5ad4aab541097ca7632ec9c73b2408bba42
sameer <pawar96sameer@gmail.com>
2025-01-03 17:59:13 +0530

    some basics of JS

--- main1.js
+++ main1.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+// Basics of JS
+console.log("Hello, world!"); //prints "Hello, world!" to the console.
+console.log(42);             // Prints number .
+console.log(true);           // boolean value.
+
+
+let greeting = "Hi";
+const pi = 3.14;     // Value cannot be changed.
+var color = "blue";  // Function-scoped.
+
+greeting = "Hello";  // Works for `let`.
+color = "red";       // Works for `var`.
+pi = 3.14159;        // Cannot reassign `const`.
