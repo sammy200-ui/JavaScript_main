f9230b53b3abac657530fe35801ebf021cc714ed
sameer <pawar96sameer@gmail.com>
2025-01-24 14:56:25 +0530

    chagnes

--- main1.js
+++ main1.js
@@ -12,6 +12,7 @@ greeting = "Hello";  // Works for let.
 color = "red";       // Works for var.
 pi = 3.14159;        // Cannot reassign const.
 
+
 // Conditional statements
 if(condition){
 // conditionals in JS
